16ac727bac415084762e43ac6ac29992a6ce9d6a
Beck-Haywood <beck.haywood@students.makeschool.com>
2019-10-15 14:55:07 -0700

    implemented tkinter for desktop app

--- run.py
+++ run.py
@@ -3,6 +3,12 @@ import numpy
 from vision import Vision
 import time
 import os
+from tkinter import *
+window = Tk()
+window.geometry("300x300")
+window.title("Computer Vision")
+title1 = Label(window,text="Image matching", font=("arial",20,"bold")).pack()
+window.mainloop()
 vision = Vision()
 
 #def find_playbutton():
@@ -12,6 +18,6 @@ vision = Vision()
 #find_playbutton()
 wat = cv2.imread(os.path.expanduser('~/dev/repos/openCV_vision/static/images/play_game.png'), 0)
 cv2.imshow('play_game.png', wat)
-cv2.waitKey(20000)
+cv2.waitKey(5000)
 cv2.destroyAllWindows()
 print(wat)
